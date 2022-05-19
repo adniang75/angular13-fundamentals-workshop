40cdd8c1de6325e1fea28d3781cafa78d45e331f
Alassane NIANG <adniang75@gmail.com>
2022-05-20 00:52:41 +0200

    Module 01 - Component Challenges

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,24 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-@Component({
-  selector: 'app-courses',
-  templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
-})
+@Component ( {
+    selector: "app-courses",
+    templateUrl: "./courses.component.html",
+    styleUrls: [ "./courses.component.scss" ],
+} )
 export class CoursesComponent implements OnInit {
-  courses = [
-    {
-      id: 1,
-      title: 'Angular 13 Fundamentals',
-      description: 'Learn the fundamentals of Angular 13',
-      percentComplete: 26,
-      favorite: true
+    // 1. Render courses in a list
+    // 2. Select a course
+    // 3.Render selected course
+
+    courses = [
+        {
+            id: 1,
+            title: "Angular 13 Fundamentals",
+            description: "Learn the fundamentals of Angular 13",
+            percentComplete: 26,
+            favorite: true,
+        },
+        {
+            id: 2,
+            title: "JavaScript The HARDEST PARTS EVER!",
+            description: "Learn the JavaScript like a pro! with Will",
+            percentComplete: 26,
+            favorite: true,
+        },
+    ];
+
+    selectedCourse = null;
+
+    constructor () {
     }
-  ];
 
-  constructor() { }
+    ngOnInit () : void {
+    }
 
-  ngOnInit(): void {
-  }
+    selectCourse ( course ) {
+        this.selectedCourse = course;
+    }
+
+    deleteCourse ( courseId ) {
+        console.log ( "DELETE COURSE", courseId );
+    }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
-})
+@Component ( {
+    selector: "app-home",
+    templateUrl: "./home.component.html",
+    styleUrls: [ "./home.component.scss" ],
+} )
 export class HomeComponent implements OnInit {
-  courseLessons = [
-    { title: 'Hello Angular' },
-    { title: 'Component Fundamentals' },
-    { title: 'Template Driven Forms' },
-    { title: 'Angular Services' },
-    { title: 'Server Communication' },
-    { title: 'Component Driven Architecture' },
-    { title: 'Angular Routing' },
-    { title: 'Unit Testing Fundamentals' },
-  ];
+    courseLessons = [
+        { title: "Hello Angular" },
+        { title: "Component Fundamentals" },
+        { title: "Template Driven Forms" },
+        { title: "Angular Services" },
+        { title: "Server Communication" },
+        { title: "Component Driven Architecture" },
+        { title: "Angular Routing" },
+        { title: "Unit Testing Fundamentals" },
+    ];
 
-  constructor() {}
+    selectedCourseLesson = null;
 
-  ngOnInit() {}
+    selectCourseLesson ( courseLesson ) {
+        this.selectedCourseLesson = courseLesson;
+    }
+
+    constructor () {
+    }
+
+    ngOnInit () {
+    }
 }
